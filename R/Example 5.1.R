3dcbfc8c1e6ddc92f8e923d7701f6fc878705ee8
Bjarke Hautop <102542670+BjarkeHautop@users.noreply.github.com>
2025-04-10 14:44:01 +0200

    Update Example 5.1.R

--- R/Example 5.1.R
+++ R/Example 5.1.R
@@ -15,6 +15,7 @@ init_state <- c(n_total - init_infected, init_infected)
 t_max <- 10 # Total number of days to simulate
 true_lambda <- 0.5 # True infection parameter
 true_gamma <- 0.2 # True removal parameter
+true_phi <- 3.5 # True dispersion parameter
 
 # --- Functions for simulating the epidemic ---
 
@@ -63,7 +64,7 @@ true_states <- simulate_epidemic(
 )
 latent_i <- true_states[, 2]
 
-observations <- rpois(length(latent_i), lambda = latent_i)
+observations <- rnbinom(length(latent_i), size = true_phi, mu = latent_i)
 
 data_sim <- data.frame(
   time = 0:t_max,
@@ -88,7 +89,7 @@ prepare_data_for_plot <- function(states, observations, t_max) {
     values_to = "count"
   )
 
-  return(data_long)
+  data_long
 }
 
 plot_epidemic_data <- function(data_long, t_max) {
@@ -125,7 +126,7 @@ data_long$state <- factor(data_long$state, levels = c("s", "i", "y"))
 plot_epidemic_data(data_long, t_max)
 
 ggsave(
-  "epidemic_data_plot.png",
+  "epidemic_data_plot_negbin.png",
   dpi = 300,
   width = 6.27,
   height = 4,
@@ -147,9 +148,24 @@ log_prior_gamma <- function(gamma) {
   extraDistr::dhnorm(gamma, sigma = 1, log = TRUE)
 }
 
+# 1 / phi ~ Normal+(0, 1)
+log_prior_phi <- function(phi) {
+  if (phi <= 0) return(-Inf)
+  xi <- 1 / phi
+  log_prior_xi <- msm::dtnorm(xi, mean = 0, sd = 1, log = TRUE)
+
+  # log_jacobian: |d(1/phi)/dphi| = 1/phi^2
+  log_jacobian <- -2 * log(phi)
+
+  log_prior_xi + log_jacobian
+}
+
+
+
 log_priors <- list(
   lambda = log_prior_lambda,
-  gamma = log_prior_gamma
+  gamma = log_prior_gamma,
+  phi = log_prior_phi
 )
 
 # --- Functions for the epidemic model ---
@@ -171,9 +187,14 @@ transition_fn_gillespie <- function(particles, lambda, gamma) {
   new_particles
 }
 
-log_likelihood_fn_epidemic <- function(y, particles) {
+log_likelihood_fn_epidemic <- function(y, particles, phi) {
   # particles is expected to be a matrix with columns (s, i)
-  dpois(y, lambda = particles[, 2], log = TRUE)
+  dnbinom(
+    y,
+    size = phi,
+    mu = particles[, 2],
+    log = TRUE
+  )
 }
 
 ###########################################
@@ -187,33 +208,41 @@ sample_lambda <- function(n) {
   rhnorm(n, sigma = 1)
 }
 
+sample_phi <- function(n) {
+  inv_phi <- msm::rtnorm(n, mean = 0, sd = 1, lower = 0)
+  1 / inv_phi
+}
+
 # Generate 20 samples and plot the prior predictive distribution along the
 # observed data
 prior_samples <- data.frame(
   lambda = sample_lambda(20),
-  gamma = sample_gamma(20)
+  gamma = sample_gamma(20),
+  phi = sample_phi(20)
 )
 
 # Simulate trajectories for the prior predictive check
 simulate_trajectory <- function(
-    prior_samples, n_total, init_infected, t_max
-) {
+    prior_samples, n_total, init_infected, t_max) {
   prior_trajectories <- list()
 
-  for (i in 1:nrow(prior_samples)) {
+  for (i in seq_len(nrow(prior_samples))) {
     lambda <- prior_samples$lambda[i]
     gamma <- prior_samples$gamma[i]
+    phi <- prior_samples$phi[i]
 
     states <- simulate_epidemic(
       n_total, init_infected, lambda, gamma, t_max
     )
 
-    noisy_infected <- rpois(length(states[, 2]), lambda = states[, 2])
+    noisy_infected <- rnbinom(
+      length(states[, 2]), size = phi, mu = states[, 2]
+    )
 
     prior_trajectories[[i]] <- noisy_infected
   }
 
-  return(prior_trajectories)
+  prior_trajectories
 }
 
 # Generate the prior predictive trajectories
@@ -224,9 +253,11 @@ prior_trajectories <- simulate_trajectory(
 plot_data <- data.frame(
   time = rep(0:t_max, times = length(prior_trajectories) + 1),
   infected = c(observations, unlist(prior_trajectories)),
-  type = rep(c(
-    "Observed",
-    paste0("Sampled ", 1:length(prior_trajectories))),
+  type = rep(
+    c(
+      "Observed",
+      paste0("Sampled ", seq_along(prior_trajectories))
+    ),
     each = t_max + 1
   )
 )
@@ -266,7 +297,7 @@ ggplot() +
 
 # Save the plot
 ggsave(
-  "prior_predictive_check_sir_example_5.1.png",
+  "prior_predictive_check_sir_example_5.1_negbin.png",
   dpi = 300,
   width = 6.27,
   height = 4,
@@ -279,47 +310,44 @@ ggsave(
 # 4. PMMH
 ###########################################
 
-result_sir_example_5.1 <- bayesSSM::pmmh(
+# Convert prior_samples to a list and use those as init_params for pilot chain.
+
+pilot_init_params <- split(prior_samples, seq(nrow(prior_samples)))
+pilot_init_params <- lapply(
+  pilot_init_params,
+  function(row) setNames(as.numeric(row), names(prior_samples))
+)
+
+# Use 4 rows of prior_samples as initial values for the pilot chain
+values <- c(1:3, 14)
+pilot_init_params <- pilot_init_params[values]
+pilot_init_params
+
+result_sir_example_5.1_negbin <- bayesSSM::pmmh(
   y = observations,
   m = 10000,
   init_fn = init_fn_epidemic,
   transition_fn = transition_fn_gillespie,
   log_likelihood_fn = log_likelihood_fn_epidemic,
   log_priors = log_priors,
-  init_params = c(lambda = 0.5, gamma = 0.5),
-  burn_in = 500,
+  pilot_init_params = pilot_init_params,
+  burn_in = 100,
   num_chains = 4,
-  param_transform = list(lambda = "log", gamma = "log"),
+  param_transform = list(lambda = "log", gamma = "log", phi = "log"),
   verbose = TRUE,
   return_latent_state_est = TRUE,
   seed = 1405,
   num_cores = 4
 )
 
-saveRDS(result_sir_example_5.1, "result_sir_example_5.1.rds")
+saveRDS(result_sir_example_5.1_negbin, "result_sir_example_5.1_negbin.rds")
 
 # Or load the result from a file
-# result_sir_example_5.1 <- readRDS("result_sir_example_5.1.rds")
+result_sir_example_5.1_negbin <- readRDS("result_sir_example_5.1_negbin.rds")
 
-chains <- result_sir_example_5.1$theta_chain
+chains <- result_sir_example_5.1_negbin$theta_chain
 
-chain_1 <- chains[[1]]
-chain_2 <- chains[[2]]
-chain_3 <- chains[[3]]
-chain_4 <- chains[[4]]
-
-lambda_chains <- cbind(
-  chain_1$lambda, chain_2$lambda, chain_3$lambda, chain_4$lambda
-)
-
-lambda_df <- data.frame(
-  value = c(
-    lambda_chains
-  ),
-  chain = factor(rep(1:4, each = nrow(lambda_chains)))
-)
-
-ggplot(lambda_df, aes(x = value, fill = chain)) +
+ggplot(chains, aes(x = lambda, fill = factor(chain))) +
   geom_density(alpha = 0.4) +
   theme_minimal(base_size = 14) +
   theme(
@@ -333,25 +361,15 @@ ggplot(lambda_df, aes(x = value, fill = chain)) +
     y = "Density"
   )
 
-ggsave("density_plot_lambda_sir.png",
-       dpi = 300,
-       width = 6.27,
-       height = 4,
-       units = "in"
+ggsave(
+  "density_plot_example_5.1_lambda_negbin.png",
+  dpi = 300,
+  width = 6.27,
+  height = 4,
+  units = "in"
 )
 
-gamma_chains <- cbind(
-  chain_1$gamma, chain_2$gamma, chain_3$gamma, chain_4$gamma
-)
-
-gamma_df <- data.frame(
-  value = c(
-    gamma_chains
-  ),
-  chain = factor(rep(1:4, each = nrow(gamma_chains)))
-)
-
-ggplot(gamma_df, aes(x = value, fill = chain)) +
+ggplot(chains, aes(x = gamma, fill = factor(chain))) +
   geom_density(alpha = 0.4) +
   theme_minimal(base_size = 14) +
   theme(
@@ -365,11 +383,34 @@ ggplot(gamma_df, aes(x = value, fill = chain)) +
     y = "Density"
   )
 
-ggsave("density_plot_gamma_sir.png",
-       dpi = 300,
-       width = 6.27,
-       height = 4,
-       units = "in"
+ggsave(
+  "density_plot_example_5.1_gamma_negbin.png",
+  dpi = 300,
+  width = 6.27,
+  height = 4,
+  units = "in"
+)
+
+ggplot(chains, aes(x = phi, fill = factor(chain))) +
+  geom_density(alpha = 0.4) +
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(size = 12),
+    panel.grid.major = element_line(linewidth = 0.5, color = "gray80"),
+    panel.grid.minor = element_blank()
+  ) +
+  labs(
+    x = "Parameter Value",
+    y = "Density"
+  )
+
+ggsave(
+  "density_plot_example_5.1_phi_negbin.png",
+  dpi = 300,
+  width = 6.27,
+  height = 4,
+  units = "in"
 )
 
 #########################################
@@ -378,16 +419,9 @@ ggsave("density_plot_gamma_sir.png",
 
 set.seed(1405)
 
-combined_df <- data.frame(
-  iteration = rep(1:nrow(lambda_chains), times = 4),
-  chain = factor(rep(1:4, each = nrow(lambda_chains))),
-  lambda = c(lambda_chains),
-  gamma = c(gamma_chains)
-)
-
-num_iterations <- 20
-sampled_df <- combined_df[
-  sample(nrow(combined_df), num_iterations, replace = TRUE),
+num_iterations <- 100
+sampled_df <- chains[
+  sample(nrow(chains), num_iterations, replace = TRUE),
 ]
 
 posterior_trajectories <- simulate_trajectory(
@@ -398,9 +432,10 @@ posterior_trajectories <- simulate_trajectory(
 plot_data_posterior <- data.frame(
   time = rep(0:t_max, times = length(posterior_trajectories) + 1),
   infected = c(observations, unlist(posterior_trajectories)),
-  type = rep(c(
-    "Observed",
-    paste0("Sampled ", 1:length(posterior_trajectories))),
+  type = rep(
+    c(
+      "Observed",
+      paste0("Sampled ", seq_along(posterior_trajectories))),
     each = t_max + 1
   )
 )
@@ -427,7 +462,7 @@ ggplot() +
     title = "Posterior Predictive Check"
   ) +
   scale_x_continuous(
-    breaks = seq(0, 10, by = 1)
+    breaks = seq(0, 14, by = 1)
   ) +
   theme_bw() +
   theme(
@@ -439,9 +474,8 @@ ggplot() +
     panel.grid = element_blank()
   )
 
-
 ggsave(
-  "posterior_predictive_check_infected_example_5.1.png",
+  "posterior_predictive_check_example_5.1_negbin.png",
   dpi = 300,
   width = 6.27,
   height = 4,
@@ -460,8 +494,8 @@ ggsave(
 results_list <- list()
 
 # Loop over each chain
-for (chain_idx in seq_along(result_sir_example_5.1$latent_state_chain)) {
-  chain <- result_sir_example_5.1$latent_state_chain[[chain_idx]]
+for (chain_idx in seq_along(result_sir_example_5.1_negbin$latent_state_chain)) {
+  chain <- result_sir_example_5.1_negbin$latent_state_chain[[chain_idx]]
 
   # Extract the [11,1] and [11,2] values for each matrix in the chain
   extracted_values <- do.call(
@@ -484,17 +518,11 @@ colnames(latent_est_df) <- c("S", "I", "chain")
 
 
 # Combine the latent state estimates with the sampled parameters
+# Add latent_est_df to `chains`
+chains$S <- latent_est_df$S
+chains$I <- latent_est_df$I
 
-combined_df <- data.frame(
-  iteration = rep(1:nrow(lambda_chains), times = 4),
-  chain = factor(rep(1:4, each = nrow(lambda_chains))),
-  lambda = c(lambda_chains),
-  gamma = c(gamma_chains),
-  S = final_df$S,
-  I = final_df$I
-)
-
-num_iterations <- dim(combined_df)[1]
+num_iterations <- nrow(chains)
 
 future_inference_df <- data.frame(
   iteration = 1:num_iterations,
@@ -503,12 +531,12 @@ future_inference_df <- data.frame(
 )
 
 for (i in 1:num_iterations) {
-  state <- as.numeric(combined_df[i, c("S", "I")])
-  lambda <- combined_df$lambda[i]
-  gamma <- combined_df$gamma[i]
+  state <- as.numeric(chains[i, c("S", "I")])
+  lambda <- chains$lambda[i]
+  gamma <- chains$gamma[i]
 
   num_days <- 10
-  while (state[2] > 0) {  # Continue until the number of infected reaches 0
+  while (state[2] > 0) { # Continue until the number of infected reaches 0
     num_days <- num_days + 1
     state <- epidemic_step(state, lambda = lambda, gamma = gamma, n_total)
   }
@@ -522,4 +550,3 @@ mean(n_total - future_inference_df$not_infected)
 # Credible interval
 quantile(future_inference_df$num_days, c(0.025, 0.975))
 quantile(n_total - future_inference_df$not_infected, c(0.025, 0.975))
-
